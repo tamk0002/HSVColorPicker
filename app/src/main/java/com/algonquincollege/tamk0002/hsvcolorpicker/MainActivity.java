3bc1c8db09dada461138f1e509be7cafa07117cf
tamk0002 <Christine@Christines-MacBook-Pro.local>
2017-10-26 02:19:54 -0400

    Initial commit

---
+++ app/src/main/java/com/algonquincollege/tamk0002/hsvcolorpicker/MainActivity.java
@@ -0,0 +1,238 @@
+package com.algonquincollege.tamk0002.hsvcolorpicker;
+
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import model.HSVModel;
+
+/**
+ * HSVModel Color Picker
+ * To implement, test and deploy a guess a HSV color picker Android app.
+ * Users can change the colorSwatch accordingly with the different seekBars (hue, saturation,
+ * and value). There are also set colour buttons at the bottom.
+ *
+ * @author Christine Tamkican (tamk0002@AlgonquinCollege.com)
+ */
+
+public class MainActivity extends Activity implements Observer, SeekBar.OnSeekBarChangeListener {
+
+
+    private static final String LOG_TAG = "HSV";
+
+    private TextView mColorSwatch;
+    private HSVModel mModel;
+    private SeekBar mHueSB;
+    private SeekBar mSaturationSB;
+    private SeekBar mValueSB;
+    private TextView mHueTV;
+    private TextView mSaturationTV;
+    private TextView mValueTV;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mModel = new HSVModel();
+        mModel.setHue(Math.round(HSVModel.MIN_HUE) * 100);
+        mModel.setSaturation(Math.round(HSVModel.MIN_SATURATION_VALUE) * 100);
+        mModel.setValue(Math.round(HSVModel.MIN_SATURATION_VALUE) * 100);
+
+        mModel.addObserver(this);
+
+
+        mColorSwatch = (TextView) findViewById(R.id.colorSwatch);
+        mHueSB = (SeekBar) findViewById(R.id.hueSB);
+        mSaturationSB = (SeekBar) findViewById(R.id.saturationSB);
+        mValueSB = (SeekBar) findViewById(R.id.valueSB);
+        mHueTV = (TextView) findViewById(R.id.hue);
+        mSaturationTV = (TextView) findViewById(R.id.saturation);
+        mValueTV = (TextView) findViewById(R.id.value);
+
+
+        mHueSB.setMax(Math.round(HSVModel.MAX_HUE));
+        mSaturationSB.setMax(Math.round(HSVModel.MAX_SATURATION_VALUE) * 100);
+        mValueSB.setMax(Math.round(HSVModel.MAX_SATURATION_VALUE) * 100);
+
+
+        mHueSB.setOnSeekBarChangeListener(this);
+        mSaturationSB.setOnSeekBarChangeListener(this);
+        mValueSB.setOnSeekBarChangeListener(this);
+
+
+        this.updateView();
+
+        mColorSwatch.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Toast.makeText(getApplicationContext(), "H: " + mModel.getHue() + "\u00B0 \nS: " + mModel.getSaturation() * 100 + "% \nV: " + mModel.getValue() * 100 + "%", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+    }
+
+
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        if (fromUser == false) {
+            return;
+        }
+
+        switch (seekBar.getId()) {
+            case R.id.hueSB:
+                mModel.setHue((float) mHueSB.getProgress());
+                mHueTV.setText(getResources().getString(R.string.hueProgress, progress).toUpperCase() + "\u00B0");
+                break;
+
+            case R.id.saturationSB:
+                mModel.setSaturation((float) mSaturationSB.getProgress() / 100);
+                mSaturationTV.setText(getResources().getString(R.string.saturationProgress, progress).toUpperCase() + "%");
+                break;
+
+            case R.id.valueSB:
+                mModel.setValue((float) mValueSB.getProgress() / 100);
+                mValueTV.setText(getResources().getString(R.string.valueProgress, progress).toUpperCase() + "%");
+                break;
+        }
+    }
+
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        switch (seekBar.getId()) {
+            case R.id.hueSB:
+                mHueTV.setText(getResources().getString(R.string.hue));
+                break;
+
+            case R.id.saturationSB:
+                mSaturationTV.setText(getResources().getString(R.string.saturation));
+                break;
+
+            case R.id.valueSB:
+                mValueTV.setText(getResources().getString(R.string.value));
+                break;
+        }
+    }
+
+    @Override
+    public void update(Observable observable, Object data) {
+        Log.i(LOG_TAG, "The color (int) is: " + mModel.getColor() + "");
+
+        this.updateView();
+    }
+
+    private void updateColorSwatch() {
+        mColorSwatch.setBackgroundColor(mModel.getColor());
+    }
+
+    private void updateHueSB() {
+        mHueSB.setProgress(Math.round(mModel.getHue()));
+    }
+
+    private void updateSaturationSB() {
+        mSaturationSB.setProgress(Math.round(mModel.getSaturation() * 100));
+    }
+
+    private void updateValueSB() {
+        mValueSB.setProgress(Math.round(mModel.getValue() * 100));
+    }
+
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.blackBtn:
+                mModel.asBlack();
+                break;
+            case R.id.redBtn:
+                mModel.asRed();
+                break;
+            case R.id.limeBtn:
+                mModel.asLime();
+                break;
+            case R.id.blueBtn:
+                mModel.asBlue();
+                break;
+            case R.id.yellowBtn:
+                mModel.asYellow();
+                break;
+            case R.id.cyanBtn:
+                mModel.asCyan();
+                break;
+            case R.id.magentaBtn:
+                mModel.asMagenta();
+                break;
+            case R.id.silverBtn:
+                mModel.asSilver();
+                break;
+            case R.id.grayBtn:
+                mModel.asGray();
+                break;
+            case R.id.maroonBtn:
+                mModel.asMaroon();
+                break;
+            case R.id.oliveBtn:
+                mModel.asOlive();
+                break;
+            case R.id.greenBtn:
+                mModel.asGreen();
+                break;
+            case R.id.purpleBtn:
+                mModel.asPurple();
+                break;
+            case R.id.tealBtn:
+                mModel.asTeal();
+                break;
+            case R.id.navyBtn:
+                mModel.asNavy();
+                break;
+            default:
+                break;
+        }
+        Toast.makeText(this, "H: " + mModel.getHue() + "\u00B0 \nS: " + mModel.getSaturation() * 100 + "% \nV: " + mModel.getValue() * 100 + "%", Toast.LENGTH_SHORT).show();
+        updateColorSwatch();
+
+    }
+
+
+    public void updateView() {
+        this.updateColorSwatch();
+        this.updateHueSB();
+        this.updateSaturationSB();
+        this.updateValueSB();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_about) {
+            DialogFragment newFragment = new AboutDialogFragment();
+            newFragment.show(getFragmentManager(), ABOUT_DIALOG_TAG);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private static final String ABOUT_DIALOG_TAG = "About Dialog";
+}
---
+++ app/src/main/java/model/HSVModel.java
@@ -0,0 +1,165 @@
+package model;
+
+import android.graphics.Color;
+
+import java.util.Observable;
+
+/**
+ * Created by Christine on 2017-10-25.
+ */
+
+public class HSVModel extends Observable {
+
+    public static final float MAX_HUE = 359;
+    public static final float MIN_HUE = 0;
+    public static final float MAX_SATURATION_VALUE = 1;
+    public static final float MIN_SATURATION_VALUE = 0;
+
+
+    private float hue;
+    private float saturation;
+    private float value;
+
+    public HSVModel() {
+        this(MAX_HUE, MAX_SATURATION_VALUE, MAX_SATURATION_VALUE);
+    }
+
+    public HSVModel(Float hue, Float saturation, Float value) {
+        super();
+
+        this.hue = hue;
+        this.saturation = saturation;
+        this.value = value;
+    }
+
+
+    public void asBlack() {
+        this.setHSV(MIN_HUE, MIN_SATURATION_VALUE, MIN_SATURATION_VALUE);
+    }
+
+    public void asRed() {
+        this.setHSV(MIN_HUE, MAX_SATURATION_VALUE, MAX_SATURATION_VALUE);
+    }
+
+    public void asLime() {
+        this.setHSV(120, MAX_SATURATION_VALUE, MAX_SATURATION_VALUE);
+    }
+
+    public void asBlue() {
+        this.setHSV(240, MAX_SATURATION_VALUE, MAX_SATURATION_VALUE);
+    }
+
+    public void asYellow() {
+        this.setHSV(60, MAX_SATURATION_VALUE, MAX_SATURATION_VALUE);
+    }
+
+    public void asCyan() {
+        this.setHSV(180, MAX_SATURATION_VALUE, MAX_SATURATION_VALUE);
+    }
+
+    public void asMagenta() {
+        this.setHSV(300, MAX_SATURATION_VALUE, MAX_SATURATION_VALUE);
+    }
+
+    public void asSilver() {
+        this.setHSV(MIN_HUE, MIN_SATURATION_VALUE, .75f);
+    }
+
+    public void asGray() {
+        this.setHSV(MIN_HUE, MIN_SATURATION_VALUE, .5f);
+    }
+
+    public void asMaroon() {
+        this.setHSV(MIN_HUE, MAX_SATURATION_VALUE, .5f);
+    }
+
+    public void asOlive() {
+        this.setHSV(60, MAX_SATURATION_VALUE, .5f);
+    }
+
+    public void asGreen() {
+        this.setHSV(120, MAX_SATURATION_VALUE, .5f);
+    }
+
+    public void asPurple() {
+        this.setHSV(300, MAX_SATURATION_VALUE, .5f);
+    }
+
+    public void asTeal() {
+        this.setHSV(180, MAX_SATURATION_VALUE, .5f);
+    }
+
+    public void asNavy() {
+        this.setHSV(240, MAX_SATURATION_VALUE, .5f);
+    }
+
+
+    //GETTERS
+    public float getHue() {
+        return hue;
+    }
+
+    public float getSaturation() {
+        return saturation;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public int getColor() {
+        return Color.HSVToColor(new float[]{hue, saturation, value});
+    }
+
+    //SETTERS
+    public void setHue(float hue) {
+        this.hue = hue;
+
+        this.updateObservers();
+        this.getColor();
+    }
+
+    public void setSaturation(float saturation) {
+        this.saturation = saturation;
+
+        this.updateObservers();
+        this.getColor();
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+
+        this.updateObservers();
+        this.getColor();
+    }
+
+
+    public void setHSV(float hue, float saturation, float value) {
+        this.hue = hue;
+        this.saturation = saturation;
+        this.value = value;
+
+        this.updateObservers();
+    }
+
+//    private void setColor() {
+//        this.color = Color.HSVToColor(new float[]{
+//                this.hue,
+//                this.saturation,
+//                this.value
+//        });
+//        this.updateObservers();
+//    }
+
+    private void updateObservers() {
+        this.setChanged();
+        this.notifyObservers();
+    }
+
+    @Override
+    public String toString() {
+        return "HSV: [Hue=" + this.hue + "\u00B0 \nSaturation=" + this.saturation + "% \nValue=" + this.value + "%]";
+    }
+
+
+}
